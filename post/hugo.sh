84558af4974a94fbe4d9b55f285f2ad18a050cde
Linner <1525811756@qq.com>
2023-07-24 16:52:21 +0800

    ADD

--- post/hugo.sh
+++ post/hugo.sh
@@ -8,7 +8,13 @@ elif [ "$1" == "new" ]
 then
     if [ "$2" != "site" ]
     then
-        hugo new content/post/$2;
+        create_info=`hugo new "content/post/$2"`;
+        echo $create_info;
+        created=`echo $create_info | grep "content/post/$2\" created"`;
+        if [[ "$created" != "" ]]
+        then
+            code "content/post/$2";
+        fi
     fi
 elif [ -z "$1" ]
 then
